41bcf20200307c46c90b3904f8727713d4b6bfbe
gazandic <gazandic@gmail.com>
2017-04-16 20:12:46 +0700

    choropleth map image feature extractor

--- extremepoints/getRGB.py
+++ extremepoints/getRGB.py
@@ -18,16 +18,18 @@ try:
     # ball = image[0:500, base:width-1]
     # image[0:500, 0:600-1] = ball
     # cv2.imwrite("try.png", image)
-    # li = set( tuple(v) for m2d in image for v in m2d )
-    for x in range(base,width-1):
-        for y in range(0,600):
-            color = str(image[y, x])
-            if not color in li:
-                li.append(color)
+    li = set( tuple(v) for m2d in image for v in m2d )
+    #for x in range(base,width-1):
+    #    for y in range(0,600):
+    #        color = str(image[y, x])
+    #        if not color in li:
+    #            li.append(color)
 except:
     print(sys.exc_info())
     print(li)
 i = 0
 for col in li:
     i += 1
-    print("("+col+","+col+"), #"+str(i))
+    cols = str(col).replace('(','[')
+    cols = cols.replace(')',']')
+    print("("+cols+","+cols+"), #"+str(i))
