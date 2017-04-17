d9325bfe9024cbca08d361188c190cb38ca4712e
fauzanriff <fauzanriff@gmail.com>
2017-04-17 21:30:50 +0700

    add public route, delete static, include frontend

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app     = express()
 var path    = require("path");
 
-app.use('/static', express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/map.html'));
