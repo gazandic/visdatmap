239e0795ca7e28aac61f9577c3bb8bb67f61ba8d
gazandic <gazandic@gmail.com>
2017-05-14 14:25:17 +0700

    autocomplete

--- extremepoints/coba2.py
+++ extremepoints/coba2.py
@@ -1,12 +1,42 @@
 import json
 
+def merge_two_dicts(x, y):
+    z = x.copy()
+    
+    # print(z[list(z.keys())[0]])
+    z.update(y)
+    return z
+
 finaldata = {}
 with open('bahasa.json') as json_data:
     database = json.load(json_data)
     for data in database:
-        s = data['code']
-        finaldata[s] = data
+        s = data['name']
+        li = list(s)
+        n = len(li) - 1
+        before = ''
+        temp = {}
+        while (n >= 0):
+            lol = {}
+            ss = li[n]
+            if before != '':
+                lol[ss] = temp.copy()
+                temp.pop(before, None)
+                temp = lol.copy()
+            else:
+                lol[ss] = data
+                temp = lol.copy()
+            before = ss
+            n -= 1
+        finaldata = merge_two_dicts(finaldata, temp)
 
-    with open('bahasaindexed.json', 'w') as bahasajson:
+        # alternate = data['alt_name']
+        #
+        # listalternate = str(alternate).split(", ")
+        # for alter in listalternate:
+        #     finaldata[alter] = data
+        # finaldata[s] = data
+
+    with open('bahasaindexedname.js', 'w') as bahasajson:
+        bahasajson.write('var indexnama = ')
         json.dump(finaldata, bahasajson)
-# s = 'putoh'
